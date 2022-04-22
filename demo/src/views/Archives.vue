01bc75368e9d0100e975e8794fb64dfe848dce18
bqb <wy1985591589@126.com>
2022-04-22 19:35:03 +0800

    第二天作业

---
+++ demo/src/views/Archives.vue
@@ -0,0 +1,126 @@
+<template>
+    <div>
+        <!-- 头 -->
+        <div class="header">
+            <div class="el-icon-back" style="font-size: 21px;"></div>
+            <div style="text-align: center;">新建居民档案</div>
+            <div></div>
+        </div>
+
+        <!-- 表单 -->
+        <van-form>
+            <van-field v-model="username" name="真实姓名" label="真实姓名" placeholder="请填写居民的真实姓名"
+                :rules="[{ required: true, message: '请填写姓名' }]" />
+            <van-field v-model="password" name="身份证号" label="身份证号" placeholder="请填写居民的身份证号"
+                :rules="[{ required: true, message: '请填写身份证号' }]" />
+            <!-- 点击出弹窗 -->
+            <van-field v-model="password" name="性别" label="性别" placeholder="请选择居民的性别" is-link @click="showPopup" />
+
+
+            <!-- 底部弹窗 -->
+            <van-popup v-model="show" position="bottom" :style="{ height: '30%' }" class="btm">
+                <div class="top1">
+                    请选择患者性别
+                </div>
+                <div class="top2">
+                    男
+                </div>
+                <div class="top2">
+                    女
+                </div>
+                <div class="top4">
+                    取消
+                </div>
+            </van-popup>
+
+            <!-- 日期 -->
+            <van-field v-model="password" name="性别" label="出生日期" placeholder="请选择居民的出生日期" is-link @click="showPopup" />
+            <van-popup v-model="show" position="bottom" :style="{ height: '30%' }" class="btm"></van-popup>
+
+            <!-- 手机号码 -->
+            <van-field v-model="password" name="手机号码" label="手机号码" placeholder="居民登陆账号，请正确填写"
+                :rules="[{ required: true, message: '请填写手机号码' }]" />
+           
+            <!-- 登录密码 -->
+            <van-field v-model="password" name="登录密码" label="登录密码" placeholder="8位以上的数字和字母组合"
+                :rules="[{ required: true, message: '请填写登录密码' }]" />
+                
+            <!-- 现居地 -->
+            <van-field v-model="password" name="现居地" label="现居地" placeholder="请填写现居地" />
+            
+            <!-- 居民标签 -->
+            <van-field v-model="password" name="居民标签" label="居民标签" placeholder="点击选择居民标签">
+                123
+            </van-field>
+            <div style="margin: 16px;">
+                <van-button round block type="info" native-type="submit">提交</van-button>
+            </div>
+
+
+        </van-form>
+
+
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            show: false,
+        }
+    },
+    methods: {
+        showPopup() {
+            this.show = true;
+        },
+        qwe(e) {
+            console.log(e);
+        }
+    },
+}
+</script>
+<style lang="scss">
+.header {
+    width: 100%;
+    line-height: 50px;
+    background-color: #fff;
+
+    div {
+        flex: 1;
+        line-height: 50px;
+    }
+}
+
+.btm {
+    border-radius: 16px 16px 0px 0px;
+    width: 100%;
+    text-align: center;
+    background-color: #f2f2f2;
+
+    .top1 {
+        font-size: 17px;
+        font-weight: 100;
+        color: #999999;
+        line-height: 55px;
+        border-bottom: 1px solid;
+    }
+
+    .top2 {
+        color: #1877f2;
+        font-size: 20px;
+        border-bottom: 1px solid #999999;
+        line-height: 65px;
+    }
+
+    .top4 {
+        line-height: 70px;
+        font-size: 22px;
+        color: #999999;
+        position: relative;
+        top: 5px;
+    }
+}
+</style>
---
+++ demo/src/views/Home.vue
@@ -0,0 +1,288 @@
+<template>
+  <div class="home">
+    <!-- 头 -->
+    <div class="header">
+      <div>
+        <img src="" alt="" />
+        <span class="ft">王明伟医生</span>
+      </div>
+      <div></div>
+      <div class="code">二维码</div>
+    </div>
+
+    <!-- 绿盒子 -->
+    <div class="box">
+      <div class="label">
+        <div>
+          <p>已签约人数</p>
+          <h2>1000</h2>
+        </div>
+        <div>
+          <p>待签约人数</p>
+          <h2>20</h2>
+        </div>
+        <div>
+          <p>居民总数</p>
+          <h2>1020</h2>
+        </div>
+      </div>
+      <div class="label">
+        <div>
+          <p>待处理服务器</p>
+          <h2>10</h2>
+        </div>
+        <div>
+          <p>服务总量</p>
+          <h2>100</h2>
+        </div>
+        <div>
+          <p>服务评价</p>
+          <h2>439</h2>
+        </div>
+      </div>
+    </div>
+    <!-- 标签头 -->
+    <div class="box2">
+      <div style="background-color: #0a71f2;" @click="archives">
+        <div></div>
+        <p>新建档案</p>
+      </div>
+      <div style="background-color: #fdd885;">
+        <div></div>
+        <p>签约管理</p>
+      </div>
+      <div style="background-color: #f47270;">
+        <div></div>
+        <p>服务管理</p>
+      </div>
+    </div>
+
+    <div class="asd"></div>
+
+    <!-- 签约申请 -->
+    <div class="application">
+      <h3>签约申请（3）</h3><span>查看全部></span></span>
+    </div>
+
+    <!-- 用户列表 -->
+    <div class="user">
+      <div class="user1">
+        <div class="ft">王小明</div>
+        <div class="aa">58岁</div>
+        <img src="../assets/logo.png" alt="" class="aa">
+        <div class="a2">待审核</div>
+      </div>
+      <div class="user2">
+        <div>身份证号</div><span class="sp"></span>
+        <div>123546465465456</div>
+      </div>
+      <div class="user2">
+        <div>签约团队</div><span class="sp"></span>
+        <div>李明明团队</div>
+      </div>
+      <div class="user2">
+        <div>服务包</div><span class="sp"></span>
+        <div>老年人服务包</div>
+      </div>
+    </div>
+
+    <!-- 服务申请 -->
+    <div class="application">
+      <h3>服务申请（8）</h3><span>查看全部></span></span>
+    </div>
+    <div class="user">
+      <div class="user1">
+        <div class="ft">王小明</div>
+        <div class="aa">58岁</div>
+        <input type="button" value="机构门诊" class="jgmz">
+        <div class="a3">待审核</div>
+      </div>
+      <div class="user2">
+        <div>服务包</div><span class="sp"></span>
+        <div>老年服务包</div>
+      </div>
+      <div class="user2">
+        <div>服务项目</div><span class="sp"></span>
+        <div>高血压健康随访</div>
+      </div>
+      <div class="user2">
+        <div>申请时间</div><span class="sp"></span>
+        <div>2020/09/09 10:09:09</div>
+      </div>
+    </div>
+
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    archives() {
+      this.$router.push("/Archives")
+    }
+  },
+};
+</script>
+<style lang="scss">
+body {
+  background-color: #f6f9fe;
+}
+
+.header {
+  display: flex;
+
+  div {
+    flex: 1;
+  }
+
+  .code {
+    text-align: center;
+  }
+
+
+}
+
+.ft {
+  font-weight: 900;
+  font-size: 19px;
+}
+
+.box {
+  width: 100%;
+  height: 255px;
+  background-color: #52d5b6;
+  margin-top: 30px;
+  border-radius: 30px;
+
+  .label {
+    display: flex;
+
+    div {
+      flex: 1;
+      text-align: center;
+      color: #fff;
+    }
+  }
+}
+
+.box2 {
+  width: 100%;
+  height: 175px;
+  border-radius: 20px;
+  position: relative;
+  background-color: #ffffff;
+  bottom: 50px;
+  display: flex;
+
+  div {
+    margin: auto;
+    border-radius: 19px;
+    width: 64px;
+    height: 64px;
+  }
+}
+
+.asd {
+  height: 25px;
+  width: 100%;
+  background-color: #fff;
+  position: relative;
+  bottom: 42px;
+  height: 20px;
+  width: 100%;
+  background-color: #fff;
+}
+
+.application {
+  background-color: #fff;
+  bottom: 30px;
+  position: relative;
+
+  h3 {
+    display: inline-block;
+
+  }
+
+  span {
+    position: relative;
+    left: 186px;
+    color: #464646;
+  }
+}
+
+.aa {
+  margin-left: 17px;
+  line-height: 30px;
+}
+
+.user {
+  width: 100%;
+  padding-top: 15px;
+  height: 130px;
+  background-color: #fff;
+  position: relative;
+  bottom: 40px;
+  margin-top: 15px;
+
+
+  .user1 {
+    display: flex;
+    width: 100%;
+
+    h2 {
+      display: inline-block;
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+      margin-top: 5px;
+    }
+
+    .a2 {
+      color: red;
+      line-height: 30px;
+      position: relative;
+      left: 170px;
+    }
+  }
+
+  .user2 {
+    display: flex;
+    line-height: 30px;
+  }
+
+}
+
+
+.sp {
+  width: 10px;
+  height: 5px;
+  ;
+}
+
+.jgmz {
+  border: none;
+  position: relative;
+  background-color: #e2e4fd;
+  color: #6c76f4;
+  left: 15px;
+}
+
+.a3 {
+  color: red;
+  line-height: 30px;
+  position: relative;
+  left: 143px;
+}
+</style>
